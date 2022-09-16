cf814c841457b68dd6b0faa82cba438bcccf1846
Roberto Prevato <roberto.prevato@gmail.com>
2022-09-16 22:56:17 +0200

    Print information

--- server/api.py
+++ server/api.py
@@ -1,3 +1,21 @@
+import os
+import platform
+import sys
+
+print("PYTHON VERSION: " + platform.python_version())
+print("CURRENT FOLDER: " + os.getcwd())
+
+blacksheep_files = [
+    os.path.join(dp, f)
+    for dp, _, filenames in os.walk(os.path.join(os.getcwd(), "blacksheep"))
+    for f in filenames
+]
+
+for item in blacksheep_files:
+    print(item)
+
+sys.path.append("/tmp/zeit-fun-c35299ddce6c5")
+
 from blacksheep import Application, Content, Response
 
 app = Application(show_error_details=True)
