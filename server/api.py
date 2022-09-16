f9f2a902976d992122477bfadbd441a8a9d0019b
Roberto Prevato <roberto.prevato@gmail.com>
2022-09-16 21:38:34 +0200

    Show error details ON

--- server/api.py
+++ server/api.py
@@ -1,13 +1,9 @@
 from blacksheep import Application, Content, Response
 
-app = Application()
+app = Application(show_error_details=True)
 get = app.router.get
 
 
 @get("/")
 async def home() -> Response:
-    return Response(
-        200,
-        content=Content(
-            b"text/plain",
-            b"Hello world :)"))
+    return Response(200, content=Content(b"text/plain", b"Hello world :)"))
